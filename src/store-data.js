9624c1a373f65712dcb50a1c441859f7943e87e0
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-16 11:14:51 -0300

    Stored the next 5 days weather info on the data object.

--- src/store-data.js
+++ src/store-data.js
@@ -24,22 +24,27 @@ function storeInfoWeatherData(fullWeatherData, fullForecastData) {
     windSpeed: fullWeatherData.wind.speed,
     humidity: fullWeatherData.main.humidity,
     forecast: {
-      today: {
-        min: 0,
-        max: 0,
-      },
-      day1: getAvarageTemp(fullForecastData.list, 0, 8),
-      day2: getAvarageTemp(fullForecastData.list, 8, 16),
-      day3: getAvarageTemp(fullForecastData.list, 16, 24),
-      day4: getAvarageTemp(fullForecastData.list, 24, 32),
-      day5: getAvarageTemp(fullForecastData.list, 32, 40),
+      day1: getParameters(fullForecastData.list, 0, 8),
+      day2: getParameters(fullForecastData.list, 8, 16),
+      day3: getParameters(fullForecastData.list, 16, 24),
+      day4: getParameters(fullForecastData.list, 24, 32),
+      day5: getParameters(fullForecastData.list, 32, 40),
     },
   };
   console.log(infoWeatherData.forecast);
   return infoWeatherData;
 }
 
-function getAvarageTemp(fullArray, indexStart, indexEnd) {
+function getParameters(fullArray, indexStart, indexEnd) {
+  const minAndMaxTemp = getMinAndMaxTemp(fullArray, indexStart, indexEnd);
+  return {
+    min: minAndMaxTemp[0],
+    max: minAndMaxTemp[1],
+    weather: fullArray[indexStart].weather[0].main,
+  };
+}
+
+function getMinAndMaxTemp(fullArray, indexStart, indexEnd) {
   const currentDayArray = fullArray.slice(indexStart, indexEnd);
   const tempArray = currentDayArray.reduce((previousValue, currentValue) => {
     previousValue.push(currentValue.main.temp);
@@ -47,7 +52,7 @@ function getAvarageTemp(fullArray, indexStart, indexEnd) {
   }, []);
   const minTemp = Math.min(...tempArray).toFixed(0);
   const maxTemp = Math.max(...tempArray).toFixed(0);
-  return { min: minTemp, max: maxTemp };
+  return [minTemp, maxTemp];
 }
 
 export { storeLocationWeatherData, storeInfoWeatherData };
